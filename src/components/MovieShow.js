56cfcd30d6fa595db261116316dd60191cb8e596
krisine <kristine.nyaga@student.moringaschool.com>
2022-10-03 18:35:09 +0300

    done

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -8,8 +8,6 @@ function MovieShow({ movies }) {
 
   return (
     <div>
-      {/* And here we access the `movieId` stored in `params` to render 
-        information about the selected movie */}
       <h3>{movies[params.movieId].title}</h3>
     </div>
   );
--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -13,7 +13,7 @@ function MoviesPage({ movies }) {
         <h3>Choose a movie from the list above</h3>
       </Route>
       <Route path={`${match.url}/:movieId`}>
-        {/* adding the movies object as a prop to MovieShow */}
+       
         <MovieShow movies={movies} />
       </Route>
     </div>
